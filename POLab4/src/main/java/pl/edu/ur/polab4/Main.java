70f1ae36d2233e257c457bb268b64fca91162022
exzhibit97 <gwizdala.patryk@gmail.com>
2018-03-20 23:35:30 +0100

    created classes, intro to get-set

--- POLab4/src/main/java/pl/edu/ur/polab4/Main.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -1,12 +1,30 @@
 package pl.edu.ur.polab4;
 
+import pl.edu.ur.polab4.obliczanieFigur.*;
+
+import java.io.Console;
+import java.util.Scanner;
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO zad 4, 5
+
+        Scanner scanner = new Scanner(System.in);
+        double skaner;
+        Szescian szescian1 = new Szescian(5);
+        szescian1.objetosc();
+
+        Kolo kolo1 = new Kolo();
+        kolo1.setPromien(Double.parseDouble(scanner.nextLine()));
+        double promien = kolo1.getPromien();
+        System.out.println("Promien: " + promien);
+        kolo1.obwod();
+        kolo1.pole();
+
+
+
     }
     
 }
---
+++ POLab4/src/main/java/pl/edu/ur/polab4/obliczanieFigur/Kolo.java
@@ -0,0 +1,31 @@
+package pl.edu.ur.polab4.obliczanieFigur;
+
+public class Kolo {
+
+    private double promien;
+
+    public Kolo() {}
+
+    public double getPromien(){
+        return this.promien;
+    }
+
+    public void setPromien(double promien){
+        this.promien = promien;
+    }
+
+
+
+    public void obwod(){
+        double pi = 3.14;
+        double obwod = 2*pi*promien;
+        System.out.println("Obwod kola wynosi: " + obwod);
+    }
+
+    public void pole(){
+        double pi = 3.14;
+        double pole = 2*pi*(promien*promien);
+        System.out.println("Pole okregu wynosi: " + pole);
+    }
+
+}
